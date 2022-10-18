42aad2ebc825af9e2ee1e02fc1d9302a339d8a8b
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-10-17 22:26:52 -0300

    up

--- curso_03/src/main/java/com/curso3/unidade5/ParallelStreamApp.java
+++ curso_03/src/main/java/com/curso3/unidade5/ParallelStreamApp.java
@@ -6,7 +6,21 @@ import java.util.List;
 public class ParallelStreamApp {
     public static void main(String[] args) {
         //colecaoStream(); // 4670 milisegundos
-        colecaoParallelStream(); // 5426
+        colecaoParallelStream(); // 5426 ???
+    }
+
+    
+
+    private static void colecaoStream() {
+        List<String> lines = new ArrayList<String>();
+        int max=1_000_000;
+        for (int i = 0; i < max; i++) {
+            lines.add("=>"+i);
+        }
+        long temp_ini = System.currentTimeMillis();
+        lines.stream().sorted().forEach(System.out::println);
+
+        System.out.println((System.currentTimeMillis() - temp_ini));
     }
 
     private static void colecaoParallelStream() {
@@ -17,18 +31,6 @@ public class ParallelStreamApp {
         }
         long temp_ini = System.currentTimeMillis();
         lines.parallelStream().sorted().forEach(System.out::println);
-
-        System.out.println((System.currentTimeMillis() - temp_ini));
-    }
-
-    private static void colecaoStream() {
-        List<String> lines = new ArrayList<String>();
-        int max=1_000_000;
-        for (int i = 0; i < max; i++) {
-            lines.add("=>"+i);
-        }
-        long temp_ini = System.currentTimeMillis();
-        lines.stream().sorted().forEach(System.out::println);
 
         System.out.println((System.currentTimeMillis() - temp_ini));
     }
